8f0746cb380de0abee7d37b16f5dd17db6c0a92b
JYOTIPATIL-hub <142783664+JYOTIPATIL-hub@users.noreply.github.com>
2024-10-27 21:31:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-function shortcut(s1, s2) {
-  // your code here
+function shortcut(str1, str2) {
+    // Check if either string is empty, return an empty string if true
+    if (str1 === "" || str2 === "") {
+        return "";
+    }
+    // Return the first character of each string combined
+    return str1[0] + str2[0];
 }
-
-// Do not change the code below.
-const s1 = prompt("Enter s1:");
-const s2 = prompt("Enter s2:");
-alert(shortcut(s1, s2));
